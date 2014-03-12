d6f9110d8316e8b86af21cab668719a5c7f41103
robi6 <robi6@users.sourceforge.net>
2014-03-12 01:05:33 +0100

    Moved ext4magic to SF

--- src/ring_buf.c
+++ src/ring_buf.c
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2010 by Roberto Maar   *
- *   robi@users.berlios.de   *
+ *   Copyright (C) 2010 by Roberto Maar                                    *
+ *   robi6@users.sf.net                                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
